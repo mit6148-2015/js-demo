192a9eabe55e3f7ad1dafbf1b6ecf66096c5fd90
Jeffrey Sun <jrsun@mit.edu>
2015-01-07 01:13:49 -0500

    added features

--- list-app.js
+++ list-app.js
@@ -1,5 +1,57 @@
 $(document).ready(function() {
-  $('#button').click(function() {
-    $('#list').append('<li>' + $('#input').val() + '</li>');
+
+  // append text to list
+  var appendText = function(text) {
+    $('#list').append('<li>' + text + '</li>');
+  }
+
+  // append input box text to list on button click
+  $('#add-item').click(function() {
+    var inputText = $('#input').val();
+    appendText(inputText);
   });
-});
+
+  // but the problem is, text doesn't go away! change appendText function
+  var appendText = function(text) {
+    $('#list').append('<li>' + text + '</li>');
+    // +++++++ clear the input
+    $('#input').val('');
+  }
+
+  // now, let's make the ENTER key work.
+  // append input box text to list on enter key
+  var ENTER_KEY = 13;
+  $('#input').keypress(function(e) {
+    if (e.which == ENTER_KEY) {
+      var inputText = $('#input').val();
+      appendText(inputText);
+    }
+  });
+
+  // remove items from list when remove button clicked
+  $('#remove-item').click(function() {
+    $('#list').children().last().remove();
+  });
+
+  // sanitize empty inputs
+  var appendText = function(text) {
+    // check if input is empty
+    if (text != '') {
+      $('#list').append('<li>' + text + '</li>');
+      $('#input').val('');      
+    }
+  }
+
+  // add highlighting on click
+  var appendText = function(text) {
+    if (text != '') {
+      var item = $('<li>' + text + '</li>');
+      // add on click handler
+      item.click(function() {
+        item.css('background-color', 'yellow');
+      });
+      $('#list').append(item);
+      $('#input').val('');
+    }
+  }
+});  
