cb8a61d5035f53835445c3cb979a745e8cbb2d04
Charles Liu <cliu2014@mit.edu>
2015-01-07 16:28:14 -0500

    Updated lecture code

--- list-app.js
+++ list-app.js
@@ -1,3 +1,39 @@
-$('#add-item').click(function() {
-  alert('hello!');
+var colors = ['red', 'blue', 'black', 'purple'];
+
+var getRandomColor = function() {
+  return colors[Math.floor(4 * Math.random())];
+};
+
+var addItem = function() {
+    // 1. read the box
+    var inputText = $('#input').val();
+    // 2. make the <li>
+    var item = $('<li />').text(inputText).css('color', getRandomColor());
+
+    item.click(function() {
+      item.toggleClass('highlighted');
+    });
+
+    $('#list').append(item);
+
+    $('#input').val('');
+
+    $('#input').focus();
+};
+
+$(document).ready(function() {
+  $('#add-item').click(function() {
+    addItem();
+  });
+
+  var ENTER_KEY = 13;
+  $('#input').keypress(function(event) {
+    if (event.which === ENTER_KEY) {
+      addItem();
+    }
+  });
+
+  $('#remove-item').click(function() {
+    $('#list').children().last().remove();
+  });
 });
